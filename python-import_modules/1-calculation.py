afd0846e1cbcd5c5fa00d729dd2fdb2bcfe027a9
RamziHkiri <6257@holbertonstudents.com>
2023-06-05 19:38:48 +0200

    number operation

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+from re import X
+
+
 if __name__ == "__main__":
     from calculator_1 import add, mul, sub, div
     a = 10
@@ -7,7 +10,7 @@ if __name__ == "__main__":
     mul_res = mul(a, b)
     sub_res = sub(a, b)
     div_res = div(a, b)
-    print(f"{a} + {b} = {add_res}")
-    print(f"{a} - {b} = {sub_res}")
-    print(f"{a} * {b} = {mul_res}")
-    print(f"{a} / {b} = {div_res}")
+    print("{} + {} = {}".format(a, b, add_res))
+    print("{} - {} = {}".format(a, b, sub_res))
+    print("{} * {} = {}".format(a, b, mul_res))
+    print("{} / {} = {}".format(a, b, div_res))
