0b5afd9e9fb9b342e6ae3b95860b446c90f69c39
RamziHkiri <6257@holbertonstudents.com>
2023-06-13 13:58:34 +0200

    print matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+
+    for row in matrix:
+        i = 0
+        for element in row:
+            if i < len(row) - 1:
+                print(element, end=" ")
+            else:
+                print(element, end="")
+            i += 1
+        print()
