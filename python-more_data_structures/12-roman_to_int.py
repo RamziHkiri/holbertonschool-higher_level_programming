38c84c8872c8e8620578e005de1385cfcc6ed230
RamziHkiri <6257@holbertonstudents.com>
2023-06-14 16:13:10 +0200

    roman into int

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if not roman_string or isinstance(roman_string, str):
+    if not roman_string or not isinstance(roman_string, str):
         return 0
+    
     roman_numbers = {"I": 1,
                      "V": 5,
                      "X": 10,
