bbc9250274c5e66e0e9e6a184d0806c4d53ad45e
RamziHkiri <6257@holbertonstudents.com>
2023-06-14 11:15:29 +0200

    add unique

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    sum = my_list[0]
+    for i in range(1, len(my_list)):
+        test = 0
+        for j in range(0, i):
+            if my_list[i] == my_list[j]:
+                test = 1
+        if test == 0:
+            sum += my_list[i]
+    return sum
