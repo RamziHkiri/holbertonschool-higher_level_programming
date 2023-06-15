f3794e757df60b9e3a4ad96a41f43be3a1820e34
RamziHkiri <6257@holbertonstudents.com>
2023-06-15 12:31:41 +0200

    safe print integer

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,9 +2,8 @@
 
 def safe_print_integer(value):
     try:
-        if type(value) is int:
-            print("{:d}".format(value))
-            return True
-    except TypeError:
-        print("{} is not an integer".format(value))
-        return False
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        pass
+    return False
