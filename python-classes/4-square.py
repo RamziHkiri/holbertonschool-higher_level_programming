f5d6ac6a67d660bb6ce16ad3ac31e95ed6888b85
RamziHkiri <6257@holbertonstudents.com>
2023-06-19 17:25:13 +0200

    create getter and setter for the field size

---
+++ python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Define Square class"""
+
+
+class Square:
+    """define fields and methods of Square"""
+
+    def __init__(self, size=0):
+        """ Square Constructor.
+
+            args:
+                size:size of the square
+        """
+        self.size = size
+
+    def size(self):
+        return self.size
+
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.size = value
+
+    def area(self):
+        return self.size * self.size
