22e75b5780daa07afe91597463d651e0696d24af
RamziHkiri <6257@holbertonstudents.com>
2023-06-19 17:04:57 +0200

    create method called Area

---
+++ python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Define Square class"""
+
+
+class Square:
+    """define fields and methods of Square"""
+
+    def __init__(self, size=0):
+        """ Square Constructor.
+
+            args:
+                size:size of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
