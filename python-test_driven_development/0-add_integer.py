7b807f111f9b8502adbad3ed0746390879961b61
RamziHkiri <6257@holbertonstudents.com>
2023-06-20 12:23:39 +0200

    fixing docstyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""Create function tha add two integers"""
+
 
 def add_integer(a, b=98):
+    """ Return sum of two integers a and b
+        args:
+            a:integer
+            b:integer
+            return: a + b
+    """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
