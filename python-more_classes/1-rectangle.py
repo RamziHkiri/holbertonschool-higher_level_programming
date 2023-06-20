1fc559f788a0ae13f7c60d1db65b7ed493fc38fd
RamziHkiri <6257@holbertonstudents.com>
2023-06-20 13:22:49 +0200

    create setter and getter for fields

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Create an empty class named Rectangle"""
+
+
+class Rectangle:
+    """define the fields and methods here"""
+
+    def __init__(self, width=0, height=0):
+        """ Rectangle Constructor.
+
+            args:
+                width:width of rectangle
+                height:height of rectangle
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__height = value
