8188f0be86905628bb3e60018216721c39f7aaa8
RamziHkiri <6257@holbertonstudents.com>
2023-06-22 13:43:54 +0200

    define area methode that raise exeption

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Create an empty class named BaseGeometry."""
+
+
+class BaseGeometry:
+    """define the fields and methods here"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
