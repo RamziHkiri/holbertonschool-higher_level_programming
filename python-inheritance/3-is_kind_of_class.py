22e43251cb512e9b2fb5e26bbe39750ba4ec3dd1
RamziHkiri <6257@holbertonstudents.com>
2023-06-22 13:11:50 +0200

    fixing docummentation

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -2,7 +2,7 @@
 """defie a methode is_same_class"""
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """returns true if object is an instance of the given class.
 
     args:
