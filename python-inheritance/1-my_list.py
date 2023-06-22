56f0e7ddfdc0b8a57cfafdd081085b539ff53983
RamziHkiri <6257@holbertonstudents.com>
2023-06-22 12:22:42 +0200

    create class mylist inherits from class list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""define class Mylist that inherite from class list"""
+
+
+class MyList(list):
+    """define fields and metod of my class"""
+
+    def print_sorted(self):
+        """print sorted list"""
+        print (sorted(self))
