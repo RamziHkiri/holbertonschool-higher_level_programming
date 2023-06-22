03f002ffe7352fcbf798277b0d227775e2fafb06
RamziHkiri <6257@holbertonstudents.com>
2023-06-22 15:36:27 +0200

    define rectangle

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Create an empty class named BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """define the fields and methods here"""
+
+    def __init__(self, width, height):
+        """ Rectangle Constructor.
+
+            args:
+                size:size of the square
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
