1df25716361faf569c5f2c170dd69f5cd91c64bd
RamziHkiri <6257@holbertonstudents.com>
2023-07-02 11:52:37 +0200

    reading from file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""define function that read data from file"""
+
 
 def read_file(filename=""):
+    """reading and printing data from file"""
+
     with open(filename, 'r', encoding="utf-8") as f:
         for line in f:
             print(line, end="")
