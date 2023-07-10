3d94c471dc5925a582937c982fdf221e55d069ae
RamziHkiri <6257@holbertonstudents.com>
2023-07-10 14:22:02 +0200

    creation of the base model

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""define a class Base"""
+
+
+class Base:
+    """creation of the classe base methods and fields"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Base Constructor.
+
+            args:
+                id: id of an instance
+        """
+        if id is None:
+            Base.__nb_objects += 1
+            id = Base.__nb_objects
+        self.__id = id
