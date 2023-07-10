a2c75e231e6e732e379bea533f953d47df497cb1
RamziHkiri <6257@holbertonstudents.com>
2023-07-10 14:46:23 +0200

    define rectangle model

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,6 +4,15 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """Creation of the Rectangle class define methods and fields
+    args:
+        width:rectangle width
+        height: rectangle height
+        x: x value
+        y: y value
+        id:the id of the rectangle instance
+    """
+
     def __init__(self, width, height, x=0, y=0, id=None):
         super.__init__(id)
         self.width = width
