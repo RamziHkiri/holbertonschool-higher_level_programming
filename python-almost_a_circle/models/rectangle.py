573b85eca59eb06e42ce4c5cbe3ba6ede7ed8d95
RamziHkiri <6257@holbertonstudents.com>
2023-07-10 15:28:05 +0200

    define rectangle model

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -15,10 +15,10 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -27,7 +27,12 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        """width setter"""
+        """width setter """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
         self.__width = value
 
     @property
@@ -37,8 +42,14 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        """height setter"""
-        self.__height = value
+        """height setter method"""
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
 
     @property
     def x(self):
@@ -48,6 +59,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """x setter"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -58,14 +73,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """y setter"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
-
-    @property
-    def id(self):
-        """id getter"""
-        return self.__id
-
-    @id.setter
-    def id(self, value):
-        """id setter"""
-        self.__id = value
