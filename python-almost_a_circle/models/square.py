e02203f0d0075553b5b63b4d1f49154adc878df3
RamziHkiri <6257@holbertonstudents.com>
2023-07-11 12:59:05 +0200

    create the square model inherits from Rectangle class

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,7 +4,7 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """d"""
+    """define the square fields and methods"""
     def __init__(self, size, x=0, y=0, id=None):
         """constructor of square object
 
