20ad03abc6b82e68a54ff18be669dd2e9ab60ed3
RamziHkiri <6257@holbertonstudents.com>
2023-07-31 18:11:56 +0200

    10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 --  a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows AS ts 
-JOIN tv_show_genres AS tg 
+INNER JOIN tv_show_genres AS tg 
 ON ts.id = tg.show_id
 ORDER BY ts.title, tg.genre_id ASC;
