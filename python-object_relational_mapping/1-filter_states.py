f044c7e4831a7a0452bb18aac373975585265348
RamziHkiri <6257@holbertonstudents.com>
2023-08-07 12:50:11 +0200

    1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -2,17 +2,13 @@
 """ Script that lists all states starting with N"""
 
 import MySQLdb
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-
-    HOST = 'localhost'
-    PORT = 3306
-    MY_USER = argv[1]
-    MY_PSWD = argv[2]
-    MY_DB = argv[3]
-    db = MySQLdb.connect(host=HOST, user=MY_USER, password=MY_PSWD,
-                         db=MY_DB, port=PORT)
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username, passwd=mysql_password, db=db_name)
     cursor = db.cursor()
     query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
     cursor.execute(query)
@@ -21,3 +17,4 @@ if __name__ == '__main__':
         print(row)
     cursor.close()
     db.close()
+
