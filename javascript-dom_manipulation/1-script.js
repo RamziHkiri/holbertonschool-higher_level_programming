a9dd5d89332b8bf37e96cbe1ee3e7f7fa19655f0
RamziHkiri <6257@holbertonstudents.com>
2023-09-11 12:06:22 +0200

    select element using the id

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,5 @@
+// select the header element using the id red_header
+header = document.querySelector('#red_header');
+
+//change the color using the selected element
+header.style.color = '#FF0000';
